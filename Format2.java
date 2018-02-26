aa4440b064b0582df1109e3c847c1d8923cd5294
yusuf69 <yousufansari11111@gmail.com>
2018-02-26 13:56:54 +0530

    I added 18th java file

---
+++ Format2.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+public class Format2
+{
+  public static void main(String [] args)
+   {
+     Scanner s=new Scanner(System.in);
+     int i,j;
+    for(i=5;i>0;i--)
+       {
+        for(j=i;j>0;j--)
+           {
+             System.out.print(j +" ");
+           }
+         System.out.println("\n");
+       }
+  }
+}
