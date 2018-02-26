40f535b683da675ca3834c5fe187ddaaa52fc5ff
yusuf69 <yousufansari11111@gmail.com>
2018-02-26 13:25:12 +0530

    I added 2nd java file

---
+++ Add.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class Add
+{
+   public static void main(String  [] args)
+   {
+    
+     int a=5,b=8;
+     int c=a+b;
+     System.out.println("the sum of "+c);
+   }
+}
+  
+   
